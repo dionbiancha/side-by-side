044cb6e99922cedb9515787d1107dcec6ee675e7
Dionei Bianchati <dion.biancha@gmail.com>
2023-02-22 20:19:53 -0300

    refact: add typescript

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Header from "./Layout/Header";
+
+function App() {
+  return <Header />;
+}
+
+export default App;
---
+++ src/Layout/Header/index.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+
+export default function Header() {
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar component="nav">
+        <Toolbar>
+          <Typography variant="h6" component="div">
+            MUI
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
